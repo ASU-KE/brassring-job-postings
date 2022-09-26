2f5eca31709f13c4f2c3403b6703b4215bfe70e9
mnthomp2 <mnthomp2@asu.edu>
2022-09-26 14:47:36 -0700

    first commit

---
+++ js/brassring_job_postings.js
@@ -0,0 +1,121 @@
+var $jq = jQuery.noConflict();
+
+jQuery(document).ready(function ($jq) {
+  $jq("#jobPostings").empty();
+
+  // Career job type dropdown trigger
+  $jq("form#jobTypeSelect").submit(function (event) {
+    event.preventDefault();
+    $jq("#jobPostings").empty();
+    if ($jq("select").first().val() == "staff") {
+      $jq(".loading-feature").show();
+      getOpportunities($jq(".staffResponse").html());
+    } else if ($jq("select").first().val() == "student") {
+      $jq(".loading-feature").show();
+      getOpportunities($jq(".staffResponse").html());
+      console.log("Selected student");
+    }
+  });
+
+  // Clean the string that is returned from Brassring, turn into XML and return only data needed.
+  function getOpportunities(xmlResponse) {
+    console.log(xmlResponse);
+    newContent = xmlResponse;
+    newContent = newContent.replaceAll("&lt;", "<");
+    newContent = newContent.replaceAll("&gt;", ">");
+    newContent = newContent.replaceAll("&amp;", "&");
+    newContent = newContent.replace(
+      '<string xmlns="http://integrationuri.org/">',
+      ""
+    );
+    newContent = newContent.replace("</string>", "");
+    newContent = newContent.replaceAll("&lt;", "<");
+    newContent = newContent.replaceAll("&gt;", ">");
+    newContent = newContent.replaceAll("&amp;", "&");
+    console.log("sonme processing");
+    console.log(newContent);
+
+    var xml = newContent,
+      xmlDoc = $jq.parseXML(xml),
+      $xml = $jq(xmlDoc),
+      $jobTitle = $xml.find('Question[Id="42323"]'),
+      $jobLink = $xml.find("JobDetailLink"),
+      $jobCloseDate = $xml.find('Question[Id="42261"]'),
+      $jobDepartment = $xml.find('Question[Id="8318"]');
+
+    var list = [];
+
+    $jobTitle.each(function (index) {
+      var holder = [
+        $jq(this).html(),
+        $jobLink.eq(index).html(),
+        $jobCloseDate.eq(index).html(),
+        $jobDepartment.eq(index).html(),
+      ];
+      list.push(holder);
+    });
+
+    list.sort();
+
+    var jobPostings = "";
+
+    let today = getTodayDate();
+
+    $jq(list).each(function (index) {
+      if (list[index][2].startsWith(today)) {
+        return true;
+      }
+      jobPostings += "<div>";
+      jobPostings +=
+        "<div><a target='_blank' href='" +
+        list[index][1] +
+        "'>" +
+        list[index][0] +
+        "</a></div>";
+      jobPostings +=
+        "<div><strong>Department:</strong> " + list[index][3] + "</div>";
+      jobPostings +=
+        "<div><strong>Close date:</strong> " + list[index][2] + "</div>";
+      jobPostings += "</div>";
+    });
+    console.log("jobPostings" + jobPostings);
+
+    $jq("#jobPostings").append(jobPostings);
+
+    $jq("#jobPostings").hide();
+
+    $jq(".loading-feature").hide();
+
+    $jq("#jobPostings").slideDown(750);
+  }
+
+  // Return today's date in d-F (e.g. 06-April) format.
+  function getTodayDate() {
+    const month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const date = new Date();
+
+    let day = date.getDate().toString();
+
+    if (day.length == 1) {
+      day = "0" + day;
+    }
+
+    let fullMonth = month[date.getMonth()];
+
+    return day + "-" + fullMonth;
+  }
+});
---
+++ src/Plugin/Block/BrassringJobPostingsBlock.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Drupal\brassring_job_postings\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Block\BlockPluginInterface;
+use GuzzleHttp\Client;
+
+/**
+ * Provides a 'Brassring Job Postings' Block.
+ *
+ * @Block(
+ *   id = "brassring_job_postings_block",
+ *   admin_label = @Translation("Brassring Job Postings block"),
+ *   category = @Translation("Brassring Job Postings"),
+ * )
+ */
+class BrassringJobPostingsBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state)
+  {
+    $form = parent::blockForm($form, $form_state);
+    $posting_title = $this->configuration['posting_title'];
+    $posting_callout = $this->configuration['posting_callout'];
+    $staff_depts = $this->configuration['staff_depts'];
+    $student_depts = $this->configuration['student_depts'];
+
+    $form['posting_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#description' => $this->t('Add a title to the section.'),
+      '#default_value' => $posting_title,
+      '#required' => true
+    ];
+
+    $form['posting_callout'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Callout'),
+      '#description' => $this->t('Add additional callout text.'),
+      '#default_value' => $posting_callout,
+      '#required' => true
+    ];
+
+    $form['staff_depts'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Staff job listing departments'),
+      '#description' => $this->t('Add comma separated departments for staff job postings.'),
+      '#default_value' => $staff_depts,
+      '#required' => true
+    ];
+
+    $form['student_depts'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Student job listing departments'),
+      '#description' => $this->t('Add comma separated departments for student job postings.'),
+      '#default_value' => $student_depts,
+      '#required' => true
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state)
+  {
+
+    parent::blockSubmit($form, $form_state);
+
+    $this->configuration['posting_title'] = $form_state->getValue('posting_title');
+    $this->configuration['posting_callout'] = $form_state->getValue('posting_callout');
+    $this->configuration['staff_depts'] = $form_state->getValue('staff_depts');
+    $this->configuration['student_depts'] = $form_state->getValue('student_depts');
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    function getBrassringAPIData($dept_list, $staff = false) {
+
+      $client = new Client();
+
+      $siteID = $staff ? "5494" : "5495";
+
+      try {
+        $response = $client->request('POST', 'https://Import.brassring.com/WebRouter/WebRouter.asmx/route',
+          [
+            'verify' => false,
+            'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+            'form_params' => [
+              'inputXml' => "<Envelope version='01.00'><Sender><Id>12345</Id><Credential>25620</Credential></Sender><TransactInfo transactId='1' transactType='data'><TransactId>01/27/2010</TransactId><TimeStamp>12:00:00 AM</TimeStamp></TransactInfo><Unit UnitProcessor='SearchAPI'><Packet><PacketInfo packetType='data'><packetId>1</packetId></PacketInfo><Payload><InputString> <ClientId>25620</ClientId><SiteId>" . $siteID . "</SiteId><PageNumber>1</PageNumber><OutputXMLFormat>0</OutputXMLFormat> <AuthenticationToken/><HotJobs/><ProximitySearch><Distance/><Measurement/><Country/><State/><City/><zipCode/></ProximitySearch><JobMatchCriteriaText/><SelectedSearchLocaleId/><Questions><Question Sortorder='ASC' Sort='No'><Id>8318</Id> <Value><![CDATA[" . $dept_list . "]]></Value></Question></Questions></InputString></Payload></Packet></Unit></Envelope>",
+            ],
+          ]
+        );
+      }
+      catch (\Exception $error) {
+        $logger = \Drupal::logger('HTTP Client error');
+        $logger->error($error->getMessage());
+      }
+
+      return $response->getBody();
+
+    }
+
+    $config = $this->getConfiguration();
+
+    $staff_postings = getBrassringAPIData($config['staff_depts'], true);
+
+    $student_postings = getBrassringAPIData($config['student_depts'], false);
+
+    $build = [];
+
+    $build = [
+      '#theme' => 'brassring_job_postings',
+      '#posting_title' => $config['posting_title'],
+      '#posting_callout' => $config['posting_callout'],
+      '#staff_postings' => $staff_postings,
+      '#student_postings' => $student_postings,
+    ];
+
+    return $build;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge() {
+    return 0;
+  }
+
+}
